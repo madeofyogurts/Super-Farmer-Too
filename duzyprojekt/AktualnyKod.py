4f0bfae7e0e80ccd16b94036c687e4e8980b0617
Zuza <56550989+madeofyogurts@users.noreply.github.com>
2022-01-13 16:30:01 +0100

    Add files via upload

---
+++ duzyprojekt/AktualnyKod.py
@@ -0,0 +1,102 @@
+import pygame
+import pygame_gui
+import inputimie as ii
+import funkcjedokodu as fdk
+
+# Generalne, potrzebne
+pygame.init()
+pygame.display.set_caption("Super Farmer") #Tytuł okienka
+t_g = fdk.tryby_gry()
+
+# Wymiary ekranu
+SCREEN_X = 800
+SCREEN_Y = 600
+
+# Ustawienie podstawowych wartości
+gameDisplay=pygame.display.set_mode((SCREEN_X,SCREEN_Y))
+clock=pygame.time.Clock()
+
+background = pygame.Surface((SCREEN_X, SCREEN_Y))
+background.fill(pygame.Color('#9CCC65'))
+
+# Kolory
+GREEN_BACKGROUND = (156, 204, 101) # Tło - #9CCC65
+BUTTON = (29, 32, 31) # Przyciski - nieużywane, jak zakodować inny kolor przycisku?
+TEXT_COLOR = (241, 191, 152) # Jak zakodować inny kolor tekstu?
+myfont = pygame.font.SysFont("monospace", 20)
+
+# Etapy gry
+Etap_Poczatek = True
+Etap_Zasady = False
+Etap_Rozgrywka = False
+Etap_Gracze = False
+
+# Przyciski
+manager = pygame_gui.UIManager((SCREEN_X, SCREEN_Y)) # UIManager kontroluje, wszystko, co dzieje się na ekranie kodu, a także aktualizuje każdą klatkę programu
+
+# Etap 1 - Początek
+Super_Farmer = myfont.render("Super Farmer", 1, (0,0,0))
+RozpocznijGre = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(250, 250, 300, 100), text="Rozpocznij Grę", manager=manager)
+ZasadyGry = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(250, 400, 300, 100), text="Zasady Gry", manager=manager)
+
+# Etap 2 - Zasady Gry
+PowrotDoMenu = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(50, 400, 300, 100), text="Powrót", manager=manager)
+pygame_gui.elements.UIButton.hide(PowrotDoMenu)
+
+# Etap 3 - Definiowanie Graczy
+Liczba_Graczy = myfont.render("Wybierz liczbę graczy:", 1, (0,0,0))
+Graczy_Dwoch = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(50, 150, 50, 50), text="2", manager=manager)
+pygame_gui.elements.UIButton.hide(Graczy_Dwoch)
+Graczy_Trzech = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(125, 150, 50, 50), text="3", manager=manager)
+pygame_gui.elements.UIButton.hide(Graczy_Trzech)
+Graczy_Czterech = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(200, 150, 50, 50), text="4", manager=manager)
+pygame_gui.elements.UIButton.hide(Graczy_Czterech)
+Graczy_Pieciu = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(275, 150, 50, 50), text="5", manager=manager)
+pygame_gui.elements.UIButton.hide(Graczy_Pieciu)
+Imiona_Graczy = []
+
+# Etap 4 - Rozpoczęcie Rozgrywki
+Rozpocznij_Gre = pygame_gui.elements.UIButton(relative_rect=pygame.Rect(250, 250, 300, 100), text="Rozpocznij Grę!", manager=manager)
+pygame_gui.elements.UIButton.hide(Rozpocznij_Gre)
+
+
+
+while True:
+    gameDisplay.blit(background, (0, 0)) # Tworzy zdefiniowane wyżej tło, umieszcza je w prawym górnym rogu
+    manager.draw_ui(gameDisplay)
+    time_delta = clock.tick(60)/1000.0
+    mouse = pygame.mouse.get_pos()
+    for event in pygame.event.get():
+        if event.type==pygame.KEYDOWN:
+            if event.key==pygame.K_ESCAPE:
+                exit()
+        if event.type == pygame.USEREVENT:
+            if event.user_type == pygame_gui.UI_BUTTON_PRESSED:
+                if event.ui_element == RozpocznijGre:
+                    t_g.etap_rozpoczecie_gry(RozpocznijGre, ZasadyGry, gameDisplay, Liczba_Graczy, Graczy_Dwoch, Graczy_Trzech, Graczy_Czterech, Graczy_Pieciu)
+                if event.ui_element == ZasadyGry:
+                    t_g.etap_zasady_gry(RozpocznijGre, ZasadyGry, PowrotDoMenu)
+                if event.ui_element == PowrotDoMenu:
+                    t_g.powrot_do_menu(RozpocznijGre, ZasadyGry, PowrotDoMenu)
+                if event.ui_element == Graczy_Dwoch:
+                    Graczy_Liczba = 2
+                    i_g = t_g.gracze_ustaleni(gameDisplay, background, Graczy_Dwoch, Graczy_Trzech, Graczy_Czterech, Graczy_Pieciu, Graczy_Liczba, Rozpocznij_Gre)
+                if event.ui_element == Graczy_Trzech:
+                    Graczy_Liczba = 3
+                    i_g = t_g.gracze_ustaleni(gameDisplay, background, Graczy_Dwoch, Graczy_Trzech, Graczy_Czterech, Graczy_Pieciu, Graczy_Liczba, Rozpocznij_Gre)
+                if event.ui_element == Graczy_Czterech:
+                    Graczy_Liczba = 4
+                    i_g = t_g.gracze_ustaleni(gameDisplay, background, Graczy_Dwoch, Graczy_Trzech, Graczy_Czterech, Graczy_Pieciu, Graczy_Liczba, Rozpocznij_Gre)
+                if event.ui_element == Graczy_Pieciu:
+                    Graczy_Liczba = 5
+                    i_g = t_g.gracze_ustaleni(gameDisplay, background, Graczy_Dwoch, Graczy_Trzech, Graczy_Czterech, Graczy_Pieciu, Graczy_Liczba, Rozpocznij_Gre)
+                if event.ui_element == Rozpocznij_Gre:
+                    quit()
+        manager.process_events(event)
+    manager.update(time_delta) # Tak często UIManager aktualizuje program
+
+    # gameDisplay.fill(GREEN_BACKGROUND)
+    t_g.wyswietlane_teksty(gameDisplay, Liczba_Graczy, Super_Farmer)
+
+    pygame.display.update()
+    clock.tick(10)
---
+++ duzyprojekt/inputimie.py
@@ -0,0 +1,93 @@
+import pygame
+import pygame_gui
+
+def wpisz_imie():
+    imie = []
+    wybor_imienia = True
+    while wybor_imienia == True:
+        for event in pygame.event.get():
+            if event.type==pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    imie.append("1")
+                elif event.key == pygame.K_2:
+                    imie.append("2")
+                elif event.key == pygame.K_3:
+                    imie.append("3")
+                elif event.key == pygame.K_4:
+                    imie.append("4")
+                elif event.key == pygame.K_5:
+                    imie.append("5")
+                elif event.key == pygame.K_6:
+                    imie.append("6")
+                elif event.key == pygame.K_7:
+                    imie.append("7")
+                elif event.key == pygame.K_8:
+                    imie.append("8")
+                elif event.key == pygame.K_9:
+                    imie.append("9")
+                elif event.key == pygame.K_0:
+                    imie.append("0")
+                elif event.key == pygame.K_q:
+                    imie.append("q")
+                elif event.key == pygame.K_w:
+                    imie.append("w")
+                elif event.key == pygame.K_e:
+                    imie.append("e")
+                elif event.key == pygame.K_r:
+                    imie.append("r")
+                elif event.key == pygame.K_t:
+                    imie.append("t")
+                elif event.key == pygame.K_y:
+                    imie.append("y")
+                elif event.key == pygame.K_u:
+                    imie.append("u")
+                elif event.key == pygame.K_i:
+                    imie.append("i")
+                elif event.key == pygame.K_o:
+                    imie.append("o")
+                elif event.key == pygame.K_p:
+                    imie.append("p")
+                elif event.key == pygame.K_a:
+                    imie.append("a")
+                elif event.key == pygame.K_s:
+                    imie.append("s")
+                elif event.key == pygame.K_d:
+                    imie.append("d")
+                elif event.key == pygame.K_f:
+                    imie.append("f")
+                elif event.key == pygame.K_g:
+                    imie.append("g")
+                elif event.key == pygame.K_h:
+                    imie.append("h")
+                elif event.key == pygame.K_j:
+                    imie.append("j")
+                elif event.key == pygame.K_k:
+                    imie.append("k")
+                elif event.key == pygame.K_l:
+                    imie.append("l")
+                elif event.key == pygame.K_z:
+                    imie.append("z")
+                elif event.key == pygame.K_x:
+                    imie.append("x")
+                elif event.key == pygame.K_c:
+                    imie.append("c")
+                elif event.key == pygame.K_v:
+                    imie.append("v")
+                elif event.key == pygame.K_b:
+                    imie.append("b")
+                elif event.key == pygame.K_n:
+                    imie.append("n")
+                elif event.key == pygame.K_m:
+                    imie.append("m")
+                    print(imie)
+                elif event.key == pygame.K_BACKSPACE: # Ten przycisk nie działa
+                    imie.remove(imie[-1])
+                    print(imie)
+                elif event.key == pygame.K_RETURN: # Ten przycisk też nie działa
+                    print(imie)
+                    imie = [''.join(imie)]
+                    print(imie)
+                    wybor_imienia = False
+    return imie
+
+            
