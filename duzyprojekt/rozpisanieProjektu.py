5dae2f3d0e2ae05c43719990a086754c4d5fba6b
Zuza <56550989+madeofyogurts@users.noreply.github.com>
2022-01-23 18:06:38 +0100

    Delete duzyprojekt directory

--- duzyprojekt/rozpisanieProjektu.py
+++
@@ -1,9 +0,0 @@
-# Najpier okienko z dwoma opcjami: Rozpoczęcie gry oraz zasady
-# Zasady: Okno jest czyszczone, pojawiają się zasady
-# Gra: Okno jest czyszczone, pojawiają się opcje: Ustalenie liczby graczy
-# Po ustaleniu liczby graczy: Ustalona jest iteracja polegająca na liczby graczy
-# Po ustaleniu iteracji: Pojawiają się licznik zwierząt (mini tabelka), a także losowane są zwierzęta
-# Po wylosowaniu zwierząt tabelka z liczbą zwierząt jest aktualizowana, gracz otrzymuje wybór pomiędzy wymianą, a zakończeniem rundy
-# Wymiana: Wymiana na zwykłe zwierzęta, wymiana na psy
-# Wymiana na zwierzęta: Zaznaczenie które zwierzę chcesz wymienić 
-# Zakończenie rundy: Runda się kończy, rozpoczyna kolejny gracz
